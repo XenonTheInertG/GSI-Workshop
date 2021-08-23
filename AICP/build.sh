aacd8a3a5b9ab803fc48e2f5ad4e3ada57d7713f
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-08-23 22:48:46 +0600

    Update build.sh

--- AICP/build.sh
+++ AICP/build.sh
@@ -21,8 +21,8 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_LINK Generic:AICP
                
     curl -sL https://git.io/file-transfer | sh
                
-    zip -r $ROM-AB-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip *-AB-*.img
-    zip -r $ROM-Aonly-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip *-Aonly-*.img
+    zip -r $ROM-AB-$sourcever2-$date2-GSI-Workshop.zip *-AB-*.img
+    zip -r $ROM-Aonly-$sourcever2-$date2-GSI-Workshop.zip *-Aonly-*.img
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
@@ -31,8 +31,8 @@ sudo ./ErfanGSIs/url2GSI.sh $ROM_LINK Generic:AICP
     SYNC_START=$(date +"%s")
     telegram -M "AICP: Upload started"
 
-    echo "::set-env name=DOWNLOAD_A::$(./transfer $MIR "$ROM-Aonly-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip" | grep -o -P '(?<=Download Link: )\S+')"
-    echo "::set-env name=DOWNLOAD_AB::$(./transfer $MIR "$ROM-AB-$sourcever2-$date2-ErfanGSI-YuMiGSI.zip" | grep -o -P '(?<=Download Link: )\S+')"
+    echo "::set-env name=DOWNLOAD_A::$(./transfer $MIR "$ROM-Aonly-$sourcever2-$date2-GSI-Workshop.zip" | grep -o -P '(?<=Download Link: )\S+')"
+    echo "::set-env name=DOWNLOAD_AB::$(./transfer $MIR "$ROM-AB-$sourcever2-$date2-GSI-Workshop.zip" | grep -o -P '(?<=Download Link: )\S+')"
 
     SYNC_END=$(date +"%s")
     SYNC_DIFF=$((SYNC_END - SYNC_START))
