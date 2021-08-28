04791550a691f1067b85b3e4f8d85d1193af061e
Mahin Ahmed <XenonTheInertG@outlook.com>
2021-08-28 11:40:16 +0600

    Update initial.sh

--- build/initial.sh
+++ build/initial.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Sync
 
-telegram -M -C "`printenv ROM_NAME` - updating tools..."
+telegram -M -C "printenv ROM_NAME - updating tools..."
 SYNC_START=$(date +"%s")
 
 sudo -E apt-get -qq update
@@ -9,4 +9,4 @@ sudo -E apt-get -qq install git openjdk-8-jdk wget expect
 
 SYNC_END=$(date +"%s")
 SYNC_DIFF=$((SYNC_END - SYNC_START))
-telegram -M -C "`printenv ROM_NAME` - tools update completed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds."
+telegram -M -C "printenv ROM_NAME - tools update completed in $((SYNC_DIFF / 60)) minute(s) and $((SYNC_DIFF % 60)) seconds."
